00ca98308a3897a39912202a20e4f24997f637e7
jwang <jwang@jw.private.k8s>
2024-04-29 06:24:05 +0000

    train more ResNet models

---
+++ resnet_params_data/_runs/run.sh
@@ -0,0 +1,31 @@
+python train_ResNet10_22_4.py &
+python train_ResNet10_22_6.py &
+python train_ResNet10_22_8.py &
+python train_ResNet10_1111_4.py &
+python train_ResNet10_1111_6.py &
+python train_ResNet10_1111_8.py &
+wait
+
+python train_ResNet12_2111_4.py &
+python train_ResNet12_2111_6.py &
+python train_ResNet12_2111_8.py &
+python train_ResNet14_222_4.py &
+python train_ResNet14_222_6.py &
+python train_ResNet14_222_8.py &
+wait
+
+python train_ResNet14_2211_4.py &
+python train_ResNet14_2211_6.py &
+python train_ResNet14_2211_8.py &
+python train_ResNet16_2221_4.py &
+python train_ResNet16_2221_6.py &
+python train_ResNet16_2221_8.py &
+wait
+
+python train_ResNet18_2222_4.py &
+python train_ResNet18_2222_6.py &
+python train_ResNet18_2222_8.py &
+python train_ResNet34_3463_4.py &
+python train_ResNet34_3463_6.py &
+python train_ResNet34_3463_8.py &
+wait
